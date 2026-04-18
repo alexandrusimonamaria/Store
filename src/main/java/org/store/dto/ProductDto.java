8bb88b2775e64175de97a4cd5a17590ddc932f22
alexandrusimonamaria <alexandrusimonamaria228@gmail.com>
2026-04-18 10:38:32 +0300

    refactor: ProducDto class

--- src/main/java/org/store/dto/ProductDto.java
+++ src/main/java/org/store/dto/ProductDto.java
@@ -1,4 +1,3 @@
 package org.store.dto;
 
-public record ProductDto(String name, String description, Double price, String category) {
-}
+public record ProductDto(String name, String description, Double price, String category) {}
