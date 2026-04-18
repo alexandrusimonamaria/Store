37dd5d95ee94662f38d2c2e3ffbede846363e779
alexandrusimonamaria <alexandrusimonamaria228@gmail.com>
2026-04-18 13:31:24 +0300

    feat: add security

--- src/main/java/org/store/config/SecurityConfig.java
+++ src/main/java/org/store/config/SecurityConfig.java
@@ -1,4 +1,68 @@
 package org.store.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final SecurityVariables securityVariables;
+
+    public SecurityConfig(SecurityVariables securityVariables) {
+        this.securityVariables = securityVariables;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/products/**", HttpMethod.GET.name())).authenticated()
+                        .requestMatchers(new AntPathRequestMatcher("/products/**", HttpMethod.POST.name())).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/products/**", HttpMethod.PATCH.name())).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/products/**", HttpMethod.DELETE.name())).hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder()
+                        .encode(securityVariables.getUserPassword()))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder()
+                        .encode(securityVariables.getAdminPassword()))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/org/store/config/SecurityVariables.java
+++ src/main/java/org/store/config/SecurityVariables.java
@@ -1,4 +1,23 @@
 package org.store.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
 public class SecurityVariables {
+
+    @Value("${spring.security.user.name}")
+    private String userName;
+
+    @Value("${spring.security.user.password}")
+    private String userPassword;
+
+    @Value("${spring.security.admin.name}")
+    private String adminName;
+
+
+    @Value("${spring.security.admin.password}")
+    private String adminPassword;
 }
